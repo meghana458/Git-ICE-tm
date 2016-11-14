b4de433d22bb2820c5b519799dfbf2f9cf57d47f
Student <s525770@ch140005m.nwmissouri.edu>
2016-11-14 16:26:27 -0600

    Initial Commit

---
+++ Git-ICE-tm/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Git-ICE-tm
+//
+//  Created by Thakkellapati,Meghana on 11/14/16.
+//  Copyright © 2016 Thakkellapati,Meghana. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Git-ICE-tm/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Git-ICE-tm
+//
+//  Created by Thakkellapati,Meghana on 11/14/16.
+//  Copyright © 2016 Thakkellapati,Meghana. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
